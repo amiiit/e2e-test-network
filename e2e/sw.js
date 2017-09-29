8b600e05ac9577cc83051285a56de19c7e5eda72
Amit Jakubowicz <a.jakubowicz@travelaudience.com>
2017-09-29 15:16:02 +0200

    Try other configuration

--- e2e/sw.js
+++ e2e/sw.js
@@ -1,4 +1,10 @@
+// This will notify all clients about the incoming fetch event
+// Understanding clients isn't relevant here, but if you're
+// curious, you can check out the docs:
+// https://developer.mozilla.org/en-US/docs/Web/API/Clients
 const notifyClients = (event, requestBody) => {
+  // there's probably no need to notify all clients, but this
+  // does no harm to us in this testing scenario.
   clients.matchAll().then(cs => {
     cs.forEach(client => {
       client.postMessage({
@@ -10,9 +16,10 @@ const notifyClients = (event, requestBody) => {
       })
     })
   })
-
 }
 
+// self is the service-worker itself
+// onfetch fires for every outgoing request from our domain
 self.onfetch = function (event) {
   if (event.request.method === 'POST') {
     event.request.json().then(requestBody => {
@@ -23,6 +30,4 @@ self.onfetch = function (event) {
   } else {
     notifyClients(event)
   }
-
-
 };
